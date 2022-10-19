9ef70396f4956a2caa85c5edc8b132a5ec072cc5
groot <diopm.contact@gmail.com>
2022-10-19 15:56:57 +0200

    feat : save and create book

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.ismin.android
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,4 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun createBook(view: View) {
+        startActivity2()
+      }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val someData = result.data?.getSerializableExtra("extra-key")
+        }
+    }
+
+
+    fun startActivity2() {
+        val intent = Intent(this, CreateBookActivity::class.java)
+        startForResult.launch(intent)
+    }
+
+
+
+
 }
